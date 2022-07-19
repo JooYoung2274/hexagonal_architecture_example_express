d1bc951254c08db5c63ad8b2d913f5a37e4f446e
JooYoung Kim <jooooo2274@gmail.com>
2022-07-19 16:50:07 +0900

    [fix] router 수정

--- src/router/connect.router.ts
+++ src/router/connect.router.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import Container from "typedi";
-import { IConnectController } from "../waitingRoom/waitingRoom.interface";
-import ConnectController from "../waitingRoom/waitingRoom.controller";
+import { IConnectController } from "../connect/connect.interface";
+import ConnectController from "../connect/connect.controller";
+import ConnectService from "../connect/connect.service";
+import ConnectRepository from "../connect/connect.repository";
 
 const router = express.Router();
-const connectController: IConnectController = Container.get(ConnectController);
+
+const connectRepository = new ConnectRepository();
+const connectService = new ConnectService(connectRepository);
+const connectController = new ConnectController(connectService);
 
 router.get("/connect", connectController.connect);
 
