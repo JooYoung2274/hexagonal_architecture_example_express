786a0e1994d70739c4a512ed06c19a78a68cfdaf
joo2274 <jooyoung@quantumuniverse.co>
2022-09-07 14:24:11 +0900

    [add] .gitignore

--- src/redis/redis.connect.ts
+++ src/redis/redis.connect.ts
@@ -1,6 +1,6 @@
-import * as redis from "redis";
-
-export const client = redis.createClient({
-  url: process.env.REDIS_URL, // "redis://localhost:6379"
-  // redis4 버전부터 host, port에서 url로 변경.
-});
+// import * as redis from "redis";
+//
+// export const client = redis.createClient({
+//   url: process.env.REDIS_URL, // "redis://localhost:6379"
+//   // redis4 버전부터 host, port에서 url로 변경.
+// });
